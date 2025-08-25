7d6f3a8477b6d4857a56a9face26017a0fe778d8
Gabriel Vinícius <gabrielvto18@gmail.com>
2025-08-25 18:37:43 -0300

    substring

---
+++ src/Easy/findTheIndexOfTheFirstOccurrenceInString_25082025.java
@@ -0,0 +1,38 @@
+package Easy;
+
+import java.util.Map;
+
+public class findTheIndexOfTheFirstOccurrenceInString_25082025 {
+
+    public static void main(String[] args) {
+
+        Map<String, String> map = Map.of(
+                "sad", "sadbutsad",
+                "letto", "leetcode",
+                "hello", "ll",
+                "a", "a",
+                "issipi", "mississippi",
+                "c", "abc"
+        );
+        for(Map.Entry<String, String> entry : map.entrySet()){
+            int idx = strStr(entry.getValue(), entry.getKey());
+            System.out.println("Needle: "+ entry.getKey() + " idx: " + idx);
+        }
+
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        if (n < m ) return -1; // haystack < needle
+        // haystack >= needle
+        for(int i = 0; i <= n - m; i ++){
+            if (haystack.charAt(i) == needle.charAt(0)){
+                if(haystack.substring(i,i+m).equals(needle)){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
